3dae6a042f636e034f249ecc45d3e8f9ef367048
User07jap <148102158+User07jap@users.noreply.github.com>
2025-04-10 08:49:01 +0800

    Add InventoryScannerApplication.java 1744246134286

---
+++ InventoryScannerApplication.java
@@ -0,0 +1,11 @@
+package com.inventory.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryScannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryScannerApplication.class, args);
+    }
+}
