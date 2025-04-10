0e99fec187b87b14b474c06958762ee04f60e560
User07jap <148102158+User07jap@users.noreply.github.com>
2025-04-10 08:49:09 +0800

    Add src/main/java/com/inventory/model/User.java 1744246142944

---
+++ src/main/java/com/inventory/model/User.java
@@ -0,0 +1,20 @@
+package com.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String username;
+    private String password;
+    
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+}
